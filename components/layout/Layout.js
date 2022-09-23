6c3f3b403c79e418983aaf05143e64135655405d
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-09-23 15:28:50 -0700

    Modified layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -6,8 +6,8 @@ import { useAuth } from "../../firebase/AuthContext";
 import { UserContext } from "../../firebase/context";
 import toast from "react-hot-toast";
 import { BiLayout } from "react-icons/bi";
+import Navbar from "./navbar";
 function Layout({ children }) {
-  const [isOpen, setOpen] = useState(false);
   const { currentUser, logout } = useAuth();
   const { aiCredits } = useContext(UserContext);
 
@@ -20,109 +20,7 @@ function Layout({ children }) {
         <div className="svg-blob4 "></div>
         <div className="svg-blob5 "></div>
       </div>
-
-      <div className="absolute top-0 left-0 z-50 scale-75">
-        <Hamburger
-          className="hamburger"
-          toggled={isOpen}
-          toggle={() => {
-            setOpen(!isOpen);
-          }}
-          // size={25}
-          easing="ease-in"
-          color="#fff"
-          label="Show menu"
-          rounded
-        />
-      </div>
-      {isOpen && (
-        <>
-          <div
-            className="fixed top-0 right-0 z-10 w-[70%] h-[100vh]  fade-effect-fast  !overflow-hidden"
-            onClick={() => {
-              setOpen(false);
-            }}
-          ></div>
-          <div className="fixed top-0 left-0 z-10 md:w-[30%] sm:w-[50%] h-[100vh] bg-gradient-to-b from-white/90 dark:from-black/90 via-sky-100/90 dark:via-sky-900/90 to-clear-bl3 dark:to-sky-700/90 fade-effect-fast  !overflow-hidden shadow-2xl drop-shadow-2xl">
-            <ul className="z-20 flex items-left mt-4 md:gap-10 sm:gap-4 flex-col  fade-effect-quick absolute  left-0 right-0 ml-auto mr-auto w-[80%]  text-2xl">
-              <li>
-                <div className="flex items-center justify-center w-full gap-2 md:mb-10 sm:mb-5 transition md:scale-100 sm:scale-[90%] md:mt-0 sm:mt-5">
-                  <img
-                    src="/bulb.svg"
-                    alt="logo"
-                    className="h-[2.2em] "
-                  />
-
-                  <h4 className="z-50 pt-4 m-0 logo fre">BMC Generator</h4>
-                </div>
-              </li>
-              <li>
-                {/*        
-        <div className="absolute top-0 z-50 left-[40em] fade-effect-quick">
-          <div className="p-4 bg-white rounded-lg shadow-lg">
-            <div className="flex items-center gap-4"> */}
-                <Link href="/ai">
-                  <a
-                    className="flex items-center group md:scale-100 sm:scale-[80%]"
-                    onClick={() => setOpen(false)}
-                  >
-                    <FaRobot className="transition duration-500 text-sky-400 group-hover:text-sky-800" />
-                    <span className="ml-2 transition nun group-hover:text-sky-800 ">
-                      AI
-                    </span>
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/ai/canvas">
-                  <a
-                    className="flex items-center group md:scale-100 sm:scale-[80%]"
-                    onClick={() => setOpen(false)}
-                  >
-                    <BiLayout className="transition duration-500 text-sky-400 group-hover:text-sky-800" />
-                    <span className="ml-2 transition nun group-hover:text-sky-800 ">
-                      Canvas Builder
-                    </span>
-                  </a>
-                </Link>
-              </li>
-
-              <li>
-                <Link href="/mrr">
-                  <a
-                    className="flex items-center group md:scale-100 sm:scale-[80%]"
-                    onClick={() => setOpen(false)}
-                  >
-                    <FaChartLine className="transition duration-500 text-sky-400 group-hover:text-sky-800" />
-                    <span className="ml-2 transition nun group-hover:text-sky-800 sm:hidden md:block">
-                      MRR Calculator
-                    </span>
-                    <span className="ml-2 transition nun group-hover:text-sky-800 sm:block md:hidden">
-                      MRR Calc
-                    </span>
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/account">
-                  <a
-                    className="flex items-center group md:scale-100 sm:scale-[80%]"
-                    onClick={() => setOpen(false)}
-                  >
-                    <FaRegUser className="transition duration-500 text-sky-400 group-hover:text-sky-800" />
-                    <span className="ml-2 transition nun group-hover:text-sky-800 ">
-                      Account
-                    </span>
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          {/* </div>
-          </div> */}
-          {/* </div> */}
-        </>
-      )}
+      <Navbar />
       <div>{children}</div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -14,82 +14,15 @@ import { react, useState } from "react";
 export default function Home() {
   const [tool, setTool] = useState(0);
   const [loggedIn, setLoggedIn] = useState(false);
-  const [toolArray, setToolArray] = useState([
-    {
-      title: "Innovation AI",
-      description:
-        "Use the power of artificial intelligence to make ideas for you.",
-      icon: <FaRobot className="scale-200" />,
-      path: "/ai",
-    },
-    {
-      title: "Cards",
-      description: "Combinatorial inspiration with random words and images.",
-      icon: <FaRandom className="scale-200" />,
-      path: "/cards",
-    },
-  ]);
-
+  
   return (
     <div className="wrapped-iframe">
-      {/* <div className="absolute flex top-1 left-1"> */}
-      {/* <button
-          className="z-50 flex items-center justify-center w-10 h-10 text-white transition rounded-full bg-sky-400 hover:scale-110 active:scale-90"
-          onClick={() => {
-            if (tool < 3) {
-              setTool(tool + 1);
-            } else {
-              setTool(0);
-            }
-          }}
-        >
-          <FaSync />
-        </button> */}
-      {/* </div> */}
-      <div className="flex justify-center w-full mt-5">
-        <p className="text-3xl text-t-bd">
-          Welcome to <span className="fre logo">BMC Generator!</span>!
-        </p>
-      </div>
       <div
         className="px-2 pb-5 
     h-full !border-0 mb-1   m-2 !mr-8 relative rounded-xl wrapped-iframe flex flex-col justify-center items-center fade-effect-quick"
       >
-        <p className="text-xl font-bold text-t-bd">Tools:</p>
-        <div className="flex flex-wrap items-center justify-center gap-3 w-[80%]">
-          {toolArray.map((tool, index) => {
-            return (
-              <ToolDiv
-                key={index}
-                title={tool.title}
-                description={tool.description}
-                path={tool.path}
-                icon={tool.icon}
-              />
-            );
-          })}
-        </div>
+       
       </div>
     </div>
-  );
-}
-
-function ToolDiv(props) {
-  return (
-    <Link href={props.path}>
-      <div className="flex flex-col items-center justify-center w-[15em] h-[16em] rounded-2xl p-5 bg-gradient-to-t from-white/90 via-sky-200/50 hover:scale-105 transition hover:ring-4 ring-2 ring-t-bl duration-500 cursor-pointer">
-        <div className="flex flex-col items-center justify-between gap-2">
-          <div className="flex flex-col items-center gap-2">
-            <div className="w-16 h-16 bg-t-bl flex items-center justify-center !text-white rounded-full">
-              {props.icon}
-            </div>
-            <div className="text-xl font-bold text-center text-t-bd fre">
-              {props.title}
-            </div>
-          </div>
-          <div className="text-center">{props.description}</div>
-        </div>
-      </div>
-    </Link>
   );
 }
