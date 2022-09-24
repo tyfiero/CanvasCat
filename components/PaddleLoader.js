74fbf7bd0cf0b7b557e1b2177e6325121f03323f
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-09-24 10:47:13 -0700

    Updated paddle loader

--- components/PaddleLoader.js
+++ components/PaddleLoader.js
@@ -6,13 +6,10 @@ function PaddleLoader() {
   return (
     <Script
       src="https://cdn.paddle.com/paddle/paddle.js"
-      // strategy="beforeInteractive"
       onLoad={(e) => {
-        // eslint-disable-next-line
         Paddle.Setup({
           vendor: Number(process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID),
         });
-        // console.log("Loaded paddle");
       }}
     />
   );
